b3892dcb440c773edbc897ca0af5e3d08bafa747
Masahito Yoshida <masahito@axsh.net>
2013-10-07 23:34:17 +0900

    refactor xexecscript.d with buildbook-rhel6.

--- vmapp/xexecscript.d/jenkins.core/build-tools.sh
+++ vmapp/xexecscript.d/jenkins.core/build-tools.sh
@@ -5,11 +5,8 @@
 #
 set -e
 
-declare chroot_dir=$1
-
 chroot $1 $SHELL -ex <<'EOS'
   yum install -y \
-    which tar \
     qemu-kvm qemu-img \
     parted kpartx \
     gcc gcc-c++ \
--- vmapp/xexecscript.d/jenkins.master/jenkins-plugin.sh
+++ vmapp/xexecscript.d/jenkins.master/jenkins-plugin.sh
@@ -5,43 +5,46 @@
 #
 set -e
 
-declare chroot_dir=$1
-
 chroot $1 $SHELL -ex <<'EOS'
   [[ -d /var/lib/jenkins/plugins ]] || mkdir -p /var/lib/jenkins/plugins
   # install git plugin
   base_url=http://updates.jenkins-ci.org/latest
   while read plugin; do
-    curl -fSkL ${base_url}/${plugin} -o /var/lib/jenkins/plugins/${plugin}
-  done < <(cat <<-_EOS_
-	build-pipeline-plugin.hpi
-	categorized-view.hpi
-	config-autorefresh-plugin.hpi
-	configurationslicing.hpi
-	config-file-provider.hpi
-	dashboard-view.hpi
-	depgraph-view.hpi
-	downstream-buildview.hpi
-	embeddable-build-status.hpi
-	git.hpi
-	git-client.hpi
-	github-api.hpi
-	github-oauth.hpi
-	global-variable-string-parameter.hpi
-	hipchat.hpi
-	greenballs.hpi
-	jobgenerator.hpi
-	join.hpi
-	managed-scripts.hpi
-	nested-view.hpi
-	parameterized-trigger.hpi
-	rbenv.hpi
-	rebuild.hpi
-	simple-theme-plugin.hpi
-	sounds.hpi
-	timestamper.hpi
-	token-macro.hpi
-	urltrigger.hpi
+    curl -fSkL ${base_url}/${plugin}.hpi -o /var/lib/jenkins/plugins/${plugin}.hpi
+  done < <(cat <<-_EOS_ | egrep -v '^#'
+	build-pipeline-plugin
+	categorized-view
+	config-autorefresh-plugin
+	configurationslicing
+	config-file-provider
+	compact-columns
+	console-column-plugin
+	cron_column
+	dashboard-view
+	depgraph-view
+	downstream-buildview
+	embeddable-build-status
+	git
+	git-client
+	github-api
+	github-oauth
+	global-variable-string-parameter
+	hipchat
+	greenballs
+	jobgenerator
+	join
+	managed-scripts
+	nested-view
+	next-executions
+	parameterized-trigger
+	rbenv
+	rebuild
+	simple-theme-plugin
+	sounds
+	timestamper
+	token-macro
+	urltrigger
+	view-job-filters
 	_EOS_
 	)
 
